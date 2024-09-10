4f8dd4cdf82d3f08cb5c43cc4f6b3c21f3d97eac
Erica The Developer <135877885+ericaverse@users.noreply.github.com>
2024-09-09 22:24:19 -0400

    Part 1

--- control.js
+++ control.js
@@ -5,7 +5,8 @@ const area = PI * radius * radius;
 const plantArea = 0.8;
 /* Where "n" is the number of plants */
 let n = 20; //`The area is starting with 20 plants.`
-let currentPlants = n * 2;
+let currentPlants = n * 2; //`The plants double in number every week.`
+
 //Part 2: Thinking Bigger
 
 //Part 3: Errors in Judgement
