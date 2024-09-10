8810e768cdf1add9647161528de74afc14a31241
Erica The Developer <135877885+ericaverse@users.noreply.github.com>
2024-09-09 22:38:58 -0400

    Part 1 --working on calculation.

--- control.js
+++ control.js
@@ -5,7 +5,13 @@ const area = PI * radius * radius;
 const plantArea = 0.8;
 /* Where "n" is the number of plants */
 let n = 20; //`The area is starting with 20 plants.`
-let currentPlants = n * 2; //`The plants double in number every week.`
+let week = 1;
+let currentPlants = n;
+//Week 1
+console.log(`Week:`, week, `Plants`, currentPlants);
+currentPlants = n * 2; //`The plants double in number every week.`
+//Week 2
+console.log(`Week:`, (week += 1), `Plants`, currentPlants);
 
 //Part 2: Thinking Bigger
 
