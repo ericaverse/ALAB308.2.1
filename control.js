6a48f13b8439974481ecbad1ea05b35f310ee1ed
Erica The Developer <135877885+ericaverse@users.noreply.github.com>
2024-09-09 22:14:33 -0400

    Part 1: Growing Pains initialized.

--- control.js
+++ control.js
@@ -1,5 +1,8 @@
-//Part 1
+//Part 1: Growing Pains
+let radius = 5;
+const PI = 3.1415;
+const area = PI * radius * radius;
 
-//Part 2
+//Part 2: Thinking Bigger
 
-//Part 3
+//Part 3: Errors in Judgement
